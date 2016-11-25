35064ffbbf14efa9f33b0ff864d20be00d298fb4
Stanislav Bochkarev <stas7775@gmail.com>
2016-11-25 18:02:38 -0500

    Added recursive implemntation. Debugging some slef intersecting problems

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ import reflex
 import cuts
 import altitude as alt
 from new_funks import pair_wise_reoptimization
+from new_funks import reopt_recursion
+
+
 
 
 DEBUG = 1
@@ -25,8 +28,22 @@ NUM_SAMPLES = 10
 RADIUS = 0.1
 LIN_PENALTY = 1.0
 ANGULAR_PENALTY = 1.0/360
-#Initialize plotting tools
-ax = splot.init_axis()
+
+
+
+
+def pretty_print_decomp(decomp):
+	print("[..] Decomposition:")
+	for idx, poly in enumerate(decomp):
+		print("%2d: "%idx),
+		boundary = poly[0]
+		for elem in boundary:
+			print("(%.2f, %.2f), "%(elem[0],elem[1])),
+		print("\n"),
+
+
+
+
 
 # Since my decomposition tehcnique is lacking, start by hard coding the polygons and decompositions
 POLY_ID = 1
@@ -36,37 +53,52 @@ orig_poly, sites, decomp = gen_poly_and_decomp(poly_id=POLY_ID)
 cell_to_site_map = assign_sites_to_polygon(decomp, sites)
 #cell_to_site_map = {0: (10,0), 1:(10,1), 2:(0,1), 3:(0,0)}
 if DEBUG: 
-	print("[..] Cell to Site Map: %s."%cell_to_site_map)
+	print("[.] Cell to Site Map: %s."%cell_to_site_map)
+
+
 
 adj_matrix = adj.get_adjacency_as_matrix(decomp)
 
 
 
-if pair_wise_reoptimization(2,1, decomp, adj_matrix, cell_to_site_map):
+M = 3
+iterations = 0
+while iterations < M:
+	iterations += 1
+
+
+
+	chi_costs = []
+	for idx, poly in enumerate(decomp):
+		cost = chi.chi(polygon=decomp[idx], init_pos=cell_to_site_map[idx],
+						radius=RADIUS, lin_penalty=LIN_PENALTY,
+						angular_penalty=ANGULAR_PENALTY)
+		chi_costs.append((idx, cost))
+	chi_costs_sorted = sorted(chi_costs, key=lambda v:v[1], reverse=True)
 	if DEBUG:
-		print("[..] Reopt is performed!")
-	mad.post_processs_decomposition(decomp)
+		print("[.] Old costs: %s"%chi_costs_sorted)
+
+	# Begin the recursion process
+	#mad.post_processs_decomposition(decomp)
 	adj_matrix = adj.get_adjacency_as_matrix(decomp)
-
-
-print("[..] Plotting the original polygon and sites.")
-splot.plot_polygon_outline(ax, orig_poly)
-splot.plot_decomposition(ax, decomp, adj_matrix, orig_poly)
-splot.plot_init_poss_and_assignment(ax, sites, cell_to_site_map, decomp)
-
-
-
-
-
-splot.display()
+	reopt_recursion(decomp, adj_matrix, chi_costs_sorted[0][0], cell_to_site_map)
+	pretty_print_decomp(decomp)
+	print("[..] Plotting the original polygon and sites.")
+	#Initialize plotting tools
+	ax = splot.init_axis()
+	splot.plot_polygon_outline(ax, orig_poly)
+	splot.plot_decomposition(ax, decomp, adj_matrix, orig_poly)
+	splot.plot_init_poss_and_assignment(ax, sites, cell_to_site_map, decomp)
+	#splot.display()
 
 
 
 
-
-
-
-
+#if pair_wise_reoptimization(2,1, decomp, adj_matrix, cell_to_site_map):
+#	if DEBUG:
+#		print("[..] Reopt is performed!")
+#	mad.post_processs_decomposition(decomp)
+#	adj_matrix = adj.get_adjacency_as_matrix(decomp)
 
 
 
--- new_funks.py
+++ new_funks.py
@@ -1,11 +1,82 @@
 from math import sqrt
 import numpy as np
 
+
+import adjacency as adj
+import min_alt_decompose as mad
 import operations as op
 import chi
 import reflex
 import cuts
 from shapely.geometry import LineString
+
+
+DEBUG = 1
+NUM_SAMPLES = 100
+RADIUS = 0.1
+LIN_PENALTY = 1.0
+ANGULAR_PENALTY = 1.0/360
+
+
+def reopt_recursion(decomp=[], adj_matrix=[], v_max_id=0, cell_to_site_map=[]):
+	"""
+	This function will recursively look for the 
+	"""
+
+	# Compute the v_max cost
+	v_max_cost = chi.chi(polygon=decomp[v_max_id], init_pos=cell_to_site_map[v_max_id],
+						radius=RADIUS, lin_penalty=LIN_PENALTY,
+						angular_penalty=ANGULAR_PENALTY)
+	if DEBUG:
+		print("[.] Cell with maximum: %d, %f"%(v_max_id, v_max_cost))
+
+
+
+	# Find adjacent cells to v_max
+	neighbors = []
+	for cell_id, cell in enumerate(adj_matrix[v_max_id]):
+		if not cell is None:
+			neighbors.append(cell_id)
+	print("[.] Neighbors: %s"%(neighbors,))
+
+
+	# Comptue the cost matrix for adjacent cells
+	n_chi_costs = []
+	for idx in neighbors:
+		cost = chi.chi(polygon=decomp[idx], init_pos=cell_to_site_map[idx],
+						radius=RADIUS, lin_penalty=LIN_PENALTY,
+						angular_penalty=ANGULAR_PENALTY)
+		n_chi_costs.append((idx, cost))
+
+	if DEBUG:
+		print("[.] Neghbours and chi: %s"%n_chi_costs)
+
+	n_chi_costs_sorted = sorted(n_chi_costs, key=lambda v:v[1], reverse=True)
+
+	
+
+	for v_i_idx, n_cost in n_chi_costs:
+
+		if n_cost < v_max_cost:
+			print("[..] Attempting %d and %d."%(v_max_id, v_i_idx))
+			#print decomp
+			if pair_wise_reoptimization(v_max_id, v_i_idx, decomp, adj_matrix, cell_to_site_map):
+				#mad.post_processs_decomposition(decomp)
+				#adj_matrix = adj.get_adjacency_as_matrix(decomp)
+				if DEBUG:
+					print("[..] Cells %d and %d reopted."%(v_max_id, v_i_idx))
+				return
+			else:
+				reopt_recursion(decomp, adj_matrix, v_i_idx, cell_to_site_map)
+
+				#mad.post_processs_decomposition(decomp)
+				#adj_matrix = adj.get_adjacency_as_matrix(decomp)
+
+
+
+
+
+
 
 
 # WIll be implementing pair-wise reoptimization step here
